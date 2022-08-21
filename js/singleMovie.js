04400e5a8c3d004edd5c4bab659d2ab7d1a5026a
shimislm <shimislm5@gmail.com>
2022-08-21 23:25:00 +0300

    singel ui update

--- js/singleMovie.js
+++ js/singleMovie.js
@@ -1,47 +1,54 @@
 const init = () => {
-    // updateLoading();
-    doApi()
-  }
-  
-  const doApi = () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    // .html?id=
-    let idMovie = urlParams.get("id");
-    console.log("im here")
-    console.log(idMovie)
-    if(idMovie){
-      let url = `http://www.omdbapi.com/?i=${idMovie}&apikey=5a292f28`;
-      fetch(url)
+  // updateLoading();
+  doApi()
+}
+
+const doApi = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  // .html?id=
+  let idMovie = urlParams.get("id");
+  console.log("im here")
+  console.log(idMovie)
+  if (idMovie) {
+    let url = `http://www.omdbapi.com/?i=${idMovie}&apikey=5a292f28`;
+    fetch(url)
       .then(resp => resp.json())
       .then(data => {
         console.log(data);
         updateUi(data);
       })
-    }
   }
-  
-  // const updateLoading = () => {
-  //   //ידאג שרק אנימציית טעינה יוצג
-  //   document.querySelector("#id_info_div").style.display = "none";
-  // }
-  
-  const updateUi = (_movieItem) => {
-    let div = document.createElement("div");
-    div.className = "bg-danger";
-    document.querySelector("#id_main").append(div);
-    div.innerHTML = 
+}
+
+// const updateLoading = () => {
+//   //ידאג שרק אנימציית טעינה יוצג
+//   document.querySelector("#id_info_div").style.display = "none";
+// }
+
+const updateUi = (_movieItem) => {
+  let div = document.createElement("div");
+  div.className = "bg-danger container";
+  document.querySelector("#id_main").append(div);
+  div.innerHTML =
     `
-            <div class="imageBox col-md-4 col-sm-6 col-8 mx-auto">
+        <div class="description row row-cols-md-2">
+            <div class="col">
+            <h2 class="display-5 fst-italic my-5">Title : ${_movieItem.Title}</h2>
+            <h5>Diractor: ${_movieItem.Director}</h5>
+            <h5>IMDB: ${_movieItem.imdbRating}</h5>
+            <h5>Language: ${_movieItem.Language}</h5>
+            <h5>First release: ${_movieItem.Released} </h5>
+            <h6 class="mb-5">Duration: ${_movieItem.Runtime}</h6>
+            <p>${_movieItem.Plot}</p>
+            </div>
+            <div class="imageBox col">
                 <img src="${_movieItem.Poster}"
-                    alt="" width="100%" height="100%">
-            </div>
-            <div class="description container">
-                <h2 class="display-5 fst-italic">Title : ${_movieItem.Title}</h2>
-                <h5>bla bla</h5>
-            </div>
+                    alt="${_movieItem.Title}" height="100%">
+                    </div>
+          </div>
     `
-    // imdbRating Year Writer Title
-  }
-  
-  
-  init();
+  // imdbRating Year Writer Title
+}
+
+
+init();
