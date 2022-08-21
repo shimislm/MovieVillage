d34e4f812e19635c60b1f094189bec7dc565d81e
shimislm <shimislm5@gmail.com>
2022-08-21 16:19:17 +0300

    update

--- js/allMovie.js
+++ js/allMovie.js
@@ -1,5 +1,17 @@
 export default class Movies{
     constructor(_parent , _item){
-        
+        this.parent= _parent;
+        this.Title = _item.Title;
+        this.Year = _item.Year;
+        this.Type = _item.Type;
+        this.Poster =_item.Poster;
+        this.id = _item.imdbID;
+        this.render();
+    }
+    render(){
+        let div = document.createElement("div")
+        div.className = "col card mb-3"
+        document.querySelector(this.parent)
+
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 const movie_ar =[];
+
+const init =()=>{
+    getAPi();
+}
+
+const getAPi =() =>{
+    // input.value="";
+    // let _searchQuery = urlParams.get("s")? urlParams.get("s") : "red"
+    // let url = `https://www.omdbapi.com/?s=${_searchQuery}&apikey=26dac8a0`;
+    let url = `https://www.omdbapi.com/?s=red&apikey=26dac8a0`
+    fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data.Search);
+      creatMoviesList(data.Search)
+    })
+    
+
+
+}
+
+
+init();
