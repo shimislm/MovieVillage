0eb2b260790ca2ae07e6777882aab3270f80876c
shimislm <shimislm5@gmail.com>
2022-08-22 11:24:57 +0300

    singel ui update

--- js/allMovie.js
+++ js/allMovie.js
@@ -21,7 +21,7 @@ export default class AllMovies{
           </div>
         `
         div.addEventListener("click",()=>{
-          location.href = `MovieVillage/pages/movie_info.html?id=${this.id}`;
+          location.href = `pages/movie_info.html?id=${this.id}`;
         })
         // 
     }
